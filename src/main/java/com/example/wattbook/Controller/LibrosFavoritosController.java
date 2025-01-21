d4a6f683181280b1e4413fc9f869d534bf0d10ae
usuario <efernandezjurado@safareyes.es>
2025-01-21 12:58:30 +0100

    Métodos nuevos + algunos testeados

--- src/main/java/com/example/wattbook/Controller/LibrosFavoritosController.java
+++ src/main/java/com/example/wattbook/Controller/LibrosFavoritosController.java
@@ -4,10 +4,7 @@ import com.example.wattbook.Entity.LibrosFavoritos;
 import com.example.wattbook.Service.ILibrosFavoritosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,15 +15,19 @@ public class LibrosFavoritosController {
     @Autowired
     private ILibrosFavoritosService librosFavoritosService;
 
-    @GetMapping("/")
+    // Funciona
+
+    @GetMapping("/listaLibros")
     public ResponseEntity<List<LibrosFavoritos>> getLibrosFavoritos() {
         List<LibrosFavoritos> librosFavoritos = librosFavoritosService.findAll();
         return ResponseEntity.ok(librosFavoritos);
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> deleteOneLibroFavorito() {
-        librosFavoritosService.deleteOne();
+    // Funciona
+
+    @DeleteMapping("borrar/{id}")
+    public ResponseEntity<Void> deleteOneLibroFavorito(@PathVariable Long id) {
+        librosFavoritosService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/wattbook/Controller/SeguidoresController.java
+++ src/main/java/com/example/wattbook/Controller/SeguidoresController.java
@@ -9,25 +9,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/seguidores")
+@RequestMapping("/seguidores")
 public class SeguidoresController {
 
     @Autowired
     private ISeguidoresService seguidoresService;
 
-    @GetMapping
+    // Funciona
+
+    @GetMapping("/listaSeguidores")
     public ResponseEntity<List<Seguidores>> getSeguidores() {
         List<Seguidores> seguidores = seguidoresService.findAll();
         return ResponseEntity.ok(seguidores);
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> deleteSeguidor() {
-        seguidoresService.deleteById();
+    // Funciona
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSeguidor(@PathVariable Long id) {
+        seguidoresService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping
+    @PostMapping("anyadirSeguidor")
     public ResponseEntity<Seguidores> addSeguidor(@RequestBody Seguidores seguidor) {
         Seguidores seguidorGuardado = seguidoresService.addSeguidor(seguidor);
         return ResponseEntity.ok(seguidorGuardado);
